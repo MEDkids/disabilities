2eff346b3a99d764720c2d038b63cccb3feb44b6
Yash <ykkawade@gmail.com>
2024-01-31 13:44:13 -0500

    Added Page 5, 6, 7

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ function nextPage(pageNumber) {
     var x = document.getElementById("page_" + pageNumber);
     var nextPageNumber = pageNumber + 1;
     var y = document.getElementById("page_" + nextPageNumber);
+
     x.style.display = 'none';
     y.style.display = 'block';
+
+    var z = document.getElementById("elevator_arrow");
+    z.style.visibility = 'hidden';
+
+    window.scroll(0,250);
+
 }
 
 function previousPage(pageNumber) {
@@ -12,4 +19,25 @@ function previousPage(pageNumber) {
     var y = document.getElementById("page_" + nextPageNumber);
     x.style.display = 'none';
     y.style.display = 'block';
+
+    var z = document.getElementById("elevator_arrow");
+    z.style.visibility = 'hidden';
+
+    window.scroll(0,0);
+
+}
+
+function showHint() {
+    var z = document.getElementById("elevator_arrow");
+    z.style.visibility = 'visible';
+}
+
+function restart() {
+    window.scroll(0,0);
+    var x = document.getElementById("page_7");
+    var y = document.getElementById("page_1");
+
+    x.style.display = 'none';
+    y.style.display = 'block';
+
 }
